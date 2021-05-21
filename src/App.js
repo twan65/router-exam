4ea2fd0ca36c3c2a51b20dc638e32f80b801fc52
twan65 <cx.twan65@gmail.com>
2021-05-21 15:52:08 +0900

    router適用

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import About from "./page/About";
+import Home from "./page/Home";
 
-function App() {
+// <Route path="パス" component={コンポーネント} />
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/page/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>紹介</h1>
+      <p>React Router基礎プロジェクト</p>
+    </div>
+  );
+};
+
+export default About;
