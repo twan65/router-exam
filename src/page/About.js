c9bd90ce7d638c9348aa1bdf038faf950c2d355b
twan65 <cx.twan65@gmail.com>
2021-05-21 22:04:29 +0900

    query parameter exam

--- src/page/About.js
+++ src/page/About.js
@@ -1,10 +1,17 @@
 import React from "react";
+import qs from "qs";
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 先端の?を省略
+  });
+  const showDetail = query.detail === "true";
+
   return (
     <div>
       <h1>紹介</h1>
-      <p>React Router基礎プロジェクト</p>
+      <p>React Router練習プロジェクトです。</p>
+      {showDetail && <p>detailをtrueに設定しました。</p>}
     </div>
   );
 };
